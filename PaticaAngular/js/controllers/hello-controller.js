0e9adb15205efc0dafb326e75285a8659b082b88
Kurostein <felipe-rastelli@hotmail.com>
2016-08-24 10:43:49 -0300

    First commit

---
+++ PaticaAngular/js/controllers/hello-controller.js
@@ -0,0 +1,16 @@
+﻿module.controller("myHelloController",
+    function ($scope) {
+        $scope.message = "Hello Angular Js!";
+        $scope.editmessage = "Write me!";
+        $scope.pessoas = [
+            { nome: "Alice", tel: "1233-3412" },
+            { nome: "Ana", tel: "2342-3122" },
+            { nome: "Pedro", tel: "3453-1231" },
+            { nome: "Mauro", tel: "4123-1231" },
+        ];
+        $scope.testChange = testChange;
+    });
+
+function testChange() {
+    this.message = "Hello Again Angular!!";
+}
---
+++ PaticaAngular/js/controllers/pizza-controller.js
@@ -0,0 +1,11 @@
+﻿module.controller("myPizzaController", function ($scope) {
+    var controller = $scope;
+
+    controller.name = "My Pizza";
+    controller.clients = [
+        { name: "Peter", tel: "1234-1231" },
+        { name: "Ann", tel: "2342-2341" },
+        { name: "Joel", tel: "1231-1241" },
+        { name: "Mary", tel: "1241-4123" }
+    ];
+});
