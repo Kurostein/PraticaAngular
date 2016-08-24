83814ea5fec83b50a2e35cfe61ad5ed41f180d7f
Kurostein <felipe-rastelli@hotmail.com>
2016-08-24 12:13:12 -0300

    Simple crud actions implemented to clients list.

--- PaticaAngular/js/controllers/pizza-controller.js
+++ PaticaAngular/js/controllers/pizza-controller.js
@@ -8,4 +8,33 @@
         { name: "Joel", tel: "1231-1241" },
         { name: "Mary", tel: "1241-4123" }
     ];
+
+    controller.add = Add;
+    controller.edit = Edit;
+    controller.save = Save;
+    controller.delete = Delete;
+
+    function Add(client) {
+        if (client.name != "" && client.tel != ""){
+            controller.clients.push(angular.copy(client));
+            delete controller.client;
+        }            
+        else
+            return;
+    }
+
+    function Edit(client) {        
+        controller.client = client;
+        controller.editing = true;
+    }
+
+    function Save(client) {
+        client = angular.copy(controller.client);
+        controller.editing = false;
+        delete controller.client;
+    }
+
+    function Delete(client) {
+        controller.clients.splice(controller.clients.indexOf(client), 1);
+    }
 });
