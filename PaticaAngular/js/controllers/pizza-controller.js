19890cb2ce94c9f0dd00e31d64996f7331119091
Rastelli <felipe-rastelli@hotmail.com>
2016-08-26 17:23:01 -0300

    Validação corrigida, usando form DOM com semantic ao invés do angular.

--- PaticaAngular/js/controllers/pizza-controller.js
+++ PaticaAngular/js/controllers/pizza-controller.js
@@ -1,87 +1,57 @@
-﻿module.controller("myPizzaController", function ($scope) {
-    var controller = $scope;
+﻿(function () {
+    'use strict';
 
-    controller.name = "My Pizza";
-    controller.clients = [
-        { name: "Peter", tel: "1234-1231" },
-        { name: "Ann", tel: "2342-2341" },
-        { name: "Joel", tel: "1231-1241" },
-        { name: "Mary", tel: "1241-4123" }
-    ];
+    module.controller("myPizzaController", function ($scope) {
+        var controller = $scope;
 
-    controller.add = Add;
-    controller.edit = Edit;
-    controller.save = Save;
-    controller.delete = Delete;
-    controller.orderBy = OrderBy;
+        var registrationForm = $('.ui.form');
 
-    controller.order = 'name';
+        controller.name = "My Pizza";
+        controller.clients = [
+            { name: "Peter", tel: "1234-1231" },
+            { name: "Ann", tel: "2342-2341" },
+            { name: "Joel", tel: "1231-1241" },
+            { name: "Mary", tel: "1241-4123" }
+        ];
+     
+        controller.add = Add;
+        controller.edit = Edit;
+        controller.save = Save;
+        controller.delete = Delete;
+        controller.orderBy = OrderBy;
 
-    function Add(client) {
-        if (client.name != "" && client.tel != ""){
-            controller.clients.push(angular.copy(client));
-            delete controller.client;            
-        }            
-        else
-            return;
-    }
-
-    function Edit(client) {        
-        controller.editing = true;
-        controller.client = client;
-    }
-
-    function Save(client) {
-        client = angular.copy(controller.client);
-        controller.editing = false;
-        delete controller.client;
-        
-    }
-
-    function Delete(client) {
-        controller.clients.splice(controller.clients.indexOf(client), 1);
-    }
-
-    function OrderBy(colName) {
-        controller.order = colName;
-        controller.reverse = !controller.reverse;
-    }
-});
-
-$(function () {
-    $(".ui.form")
-        .form({
-            inline: true,
-            on: 'blur',
-            fields: {                
-                name: {
-                    rules: [
-                        {
-                            type: 'empty',
-                            prompt: 'The field {name} cannot be empty.'
-                        },
-                        {
-                            type: 'minLength[3]',
-                            ruleValue: 3
-                        }
-                    ]
-                },
-                tel: {
-                    rules: [
-                        {
-                            type: 'empty',
-                            prompt: 'The field {name} cannont be empty.'
-                        },
-                        {
-                            type: 'minLength[9]',
-                            ruleValue: 9
-                        },
-                        {
-                            type: 'maxLength[10]',
-                            ruleValue: 10
-                        }
-                    ]
-                }
+        controller.order = 'name';
+    
+        function Add() {
+            if (registrationForm.form('is valid')) {
+                var client = registrationForm.form('get values');
+                controller.clients.push(client);
+                registrationForm.form('clear');
             }
-        });
-});
+            else
+                return;
+        }
+
+        function Edit(client) {
+            controller.editing = true;
+            controller.client = client;
+            registrationForm.form('set values', client);
+        }
+
+        function Save(client) {        
+            controller.clients.splice(controller.clients.indexOf(client), 1, registrationForm.form('get values'));
+            registrationForm.form('clear');
+            controller.editing = false;
+            delete controller.client;
+        }
+
+        function Delete(client) {
+            controller.clients.splice(controller.clients.indexOf(client), 1);
+        }
+
+        function OrderBy(colName) {
+            controller.order = colName;
+            controller.reverse = !controller.reverse;
+        }
+    });
+})();
---
+++ PaticaAngular/js/validation/form-validation.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $(".ui.form")
+        .form({
+            inline: true,
+            on: 'blur',
+            fields: {
+                name: {
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'The field {name} cannot be empty.'
+                        },
+                        {
+                            type: 'minLength[3]',
+                            ruleValue: 3
+                        }
+                    ]
+                },
+                tel: {
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'The field {name} cannont be empty.'
+                        },
+                        {
+                            type: 'minLength[9]',
+                            ruleValue: 9
+                        },
+                        {
+                            type: 'maxLength[10]',
+                            ruleValue: 10
+                        }
+                    ]
+                }
+            }
+        });
+});
