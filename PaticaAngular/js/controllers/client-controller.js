992301f8d668904be9aaf95d4d8576d60a0f710c
Rastelli <felipe-rastelli@hotmail.com>
2016-09-06 12:12:09 -0300

    Directives training.

---
+++ PaticaAngular/js/controllers/client-controller.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    const ClientController = function ($scope, $http, $q) {
+        var vm = this;
+        var url = 'http://localhost:2020/';
+
+        vm.client = { name: 'test', tel: '123123123'};
+
+        vm.add = Add;
+
+        function Add(client) {
+            //if (registrationForm.form('is valid')) {
+            //    var client = registrationForm.form('get values');
+            //var client = vm.client;
+                PostClient(client, 'client')
+                    .then(function (status) {
+                        if (status === 200) {
+                            //controller.clients.push(client);
+                            //registrationForm.form('clear');
+                            //ListClients();
+                        } else {
+                            alert('Error to insert client.');
+                            //ListClients();
+                        }
+                    });
+            //} else
+            //    return;
+        }
+
+        function PostClient(client, serviceUrl) {
+            var json = JSON.stringify(client);
+            var deferred = $q.defer();
+
+            $http.post(url.concat(serviceUrl), json)
+            .success(function (data, status) {
+                deferred.resolve(status);
+                console.log('Deu certo!!');
+            }).error(function (data, status) {
+                deferred.reject(status);
+                console.log('Deu erro...');
+            });
+
+            return deferred.promise;
+        }
+    }
+
+    ClientController.$inject = ['$scope', '$http', '$q'];
+
+    angular.module('myPizza').controller('clientController', ClientController);
+})();
---
+++ PaticaAngular/js/directives/client.form.directive.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    const ClientForm = function () {
+        return {
+            scope: {},
+            templateUrl: './js/directives/views/client.form.view.html',
+            //controller: 'clientController as vm',
+            link: Link
+        };
+
+        function Link(scope, element, attrs, clientController) {
+            ModalEvents('.add.button');
+            ModalEvents('.edit.button');
+            //$(element).modal('attach events', '.edit.button', 'show');
+            scope.client = clientController.client;
+        }
+
+        function ModalEvents(attrs) {
+            $(element)
+                .modal({
+                    closable: false,
+                    onDeny: function () { console.log('cancelou'); },
+                    onApprove: clientController.add(scope.client)
+                })
+                .modal('attach events', attrs, 'show');
+        }
+    }
+
+    angular.module('myPizza').directive('clientForm', ClientForm);
+})();
